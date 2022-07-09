c020cf44b551e7e16ef3552710a1480552d84585
skyxcripto <producer@skynance.net>
2022-07-09 11:36:03 -0300

    feat: create template for vite react-ts

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'preact/hooks'
+import viteLogo from '/vite.svg'
+import preactLogo from './assets/preact.svg'
+import './app.css'
+
+export function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
